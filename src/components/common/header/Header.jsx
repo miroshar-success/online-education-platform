7a39b1f300c6cac995fe9cefe6730fe911fd0026
oskarmast <oskarmast9436@gmail.com>
2023-04-08 07:23:41 +0300

    header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "./Head";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
   return (
     <>
       <Head />
+      <header>
+        <nav className="flexSB">
+          <ul
+            className={click ? "mobile-nav" : "flexSB"}
+            onClick={() => setClick(false)}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/courses">All Course</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Pricing</Link>
+            </li>
+            <li>
+              <Link to="/journal">Journal</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div className="start">
+            <div className="button">GET CERTIFICATE</div>
+          </div>
+          <button className="toggle" onClick={() => setClick(!click)}>
+            {click ? (
+              <i className="fa fa-times"></i>
+            ) : (
+              <i className="fa fa-bars"></i>
+            )}
+          </button>
+        </nav>
+      </header>
     </>
   );
 };
