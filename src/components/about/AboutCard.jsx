ef91116c2eba6aff57eed69633fa6cbcb4d6849d
oskarmast <oskarmast9436@gmail.com>
2023-04-09 05:45:51 +0300

    AboutCard

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Heading from "../common/heading/Heading";
+import "./about.css";
+import { homeAbout } from "../../dummydata";
+import Awrapper from "./Awrapper";
 
 const AboutCard = () => {
   return (
-    <div>AboutCard</div>
-  )
-}
+    <>
+      <section className="aboutHome">
+        <div className="container flexSB">
+          <div className="left row">
+            <img src="./images/about.webp" alt="" />
+          </div>
+          <div className="right row">
+            <Heading
+              subtitle="LEARN ANYTHING"
+              title="Benefits About Online Learning Expertise"
+            />
+            <div className="items">
+              {homeAbout.map((val) => {
+                return (
+                  <div className="item flexSB">
+                    <div className="img">
+                      <img src={val.cover} alt="" />
+                    </div>
+                    <div className="text">
+                      <h2>{val.title}</h2>
+                      <p>{val.desc}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default AboutCard
+export default AboutCard;
---
+++ src/components/about/Awrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Awrapper = () => {
+  return (
+    <div>Awrapper</div>
+  )
+}
+
+export default Awrapper
