059d50ca8e96fd5ae380138f7dc872d1c078d8ee
oskarmast <oskarmast9436@gmail.com>
2023-04-09 05:52:35 +0300

    awrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Header from './components/common/header/Header'
-import Footer from './components/common/footer/Footer'
-import Home from './components/home/Home'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/common/header/Header";
+import Footer from "./components/common/footer/Footer";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 const App = () => {
   return (
@@ -11,12 +12,13 @@ const App = () => {
       <Router>
         <Header />
         <Switch>
-          <Route exact path='/' component={Home}  />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
         </Switch>
         <Footer />
       </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
