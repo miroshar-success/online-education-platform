11474ec425f86fabe02b1acf49393437ba6a6937
oskarmast <oskarmast9436@gmail.com>
2023-04-10 14:45:13 +0300

    coursesCardFix

--- src/components/allcourse/CoursesCard.jsx
+++ src/components/allcourse/CoursesCard.jsx
@@ -1,30 +1,59 @@
 import React from "react";
 import "./courses.css";
-import { online } from "../../dummydata";
-import Heading from "../common/heading/Heading";
+import { coursesCard } from "../../dummydata";
 
-const OnlineCourses = () => {
+const CoursesCard = () => {
   return (
     <>
-      <section className="online">
-        <div className="container">
-          <Heading subtitle="COURSES" title="Browse Our Online Courses" />
-          <div className="content grid3">
-            {online.map((val) => (
-              <div className="box">
-                <div className="img">
-                  <img src={val.cover} />
-                  <img src={val.hoverCover} alt="" className="show" />
+      <section className="coursesCard">
+        <div className="container grid2">
+          {coursesCard.map((val) => (
+            <div className="items">
+              <div className="content flex">
+                <div className="left">
+                  <div className="img">
+                    <img src={val.cover} alt="" />
+                  </div>
                 </div>
-                <h1>{val.courseName}</h1>
-                <span>{val.course}</span>
+                <div className="text">
+                  <h1>{val.coursesName}</h1>
+                  <div className="rate">
+                    <i className="fa fa-start"></i>
+                    <i className="fa fa-start"></i>
+                    <i className="fa fa-start"></i>
+                    <i className="fa fa-start"></i>
+                    <i className="fa fa-start"></i>
+                    <label htmlFor="">(5.0)</label>
+                  </div>
+                  <div className="details">
+                    {val.courTeacher.map((details) => (
+                      <>
+                        <div className="box">
+                          <div className="dimg">
+                            <img src={details.dcover} alt="" />
+                          </div>
+                          <div className="para">
+                            <h4>{details.name}</h4>
+                          </div>
+                        </div>
+                        <span>{details.totalTime}</span>
+                      </>
+                    ))}
+                  </div>
+                </div>
               </div>
-            ))}
-          </div>
+              <div className="price">
+                <h3>
+                  {val.priceAll} / {val.pricePer}
+                </h3>
+              </div>
+              <button className="outline-btn">ENROLL NOW !</button>
+            </div>
+          ))}
         </div>
       </section>
     </>
   );
 };
 
-export default OnlineCourses;
+export default CoursesCard;
