26810936ecdf76ccb882b44f6d0d9ac9658c87a3
oskarmast <oskarmast9436@gmail.com>
2023-04-10 13:11:10 +0300

    pricing

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Back from "../common/back/Back";
+import PriceCard from "./PriceCard";
+import "./price.css";
+import Faq from "./Faq";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <>
+      <Back title="Choose The Right Plan" />
+      <section className="price padding">
+        <div className="container grid">
+          <PriceCard />
+        </div>
+      </section>
+      <Faq />
+    </>
+  );
+};
 
-export default Pricing
+export default Pricing;
