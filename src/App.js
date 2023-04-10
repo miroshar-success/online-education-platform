82a0c97a631df78f42493fabb4b6114acd4d22f2
oskarmast <oskarmast9436@gmail.com>
2023-04-10 14:26:16 +0300

    blog

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/common/header/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/about/About";
 import Pricing from "./components/pricing/Pricing";
+import Blog from "./components/blog/Blog";
 import Footer from "./components/common/footer/Footer";
 import Home from "./components/home/Home";
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/pricing" component={Pricing} />
+          <Route exact path="/journal" component={Blog} />
         </Switch>
         <Footer />
       </Router>
---
+++ src/components/blog/BlogCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogCard = () => {
+  return (
+    <div>BlogCard</div>
+  )
+}
+
+export default BlogCard
