794ea5de0cac0aab4440d781c5aa07a11be27205
oskarmast <oskarmast9436@gmail.com>
2023-04-10 13:07:37 +0300

    hprice

--- src/components/home/Hprice.jsx
+++ src/components/home/Hprice.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Heading from "../common/heading/Heading";
+import PriceCard from "../pricing/PriceCard";
 
 const Hprice = () => {
   return (
-    <div>Hprice</div>
-  )
-}
+    <>
+      <section className="hprice padding">
+        <Heading subtitle="OUR PRICING" title="Pricing & Packages" />
+        <div className="price container grid">
+          <PriceCard />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Hprice
+export default Hprice;
